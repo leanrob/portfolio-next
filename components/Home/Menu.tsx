d69664d3135ffa69476f6e0bc5dc372d710641e5
Rob Byrne <rob@leanrob.com>
2021-11-07 01:14:14 -0800

    Remove menu items

--- components/Home/Menu.tsx
+++ components/Home/Menu.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 import { MenuContainer, MenuItems, Item } from './HomeStyles'
 
 const Menu = ({ data }: any) => {
-    const menuItems = data.map((item: any, index: number) => {
-        return (
-            <Item key={index}>
-                <a href={item.link}>{item.title}</a>
-            </Item>
-        )
-    })
+    // const menuItems = data.map((item: any, index: number) => {
+    //     return (
+    //         <Item key={index}>
+    //             <a href={item.link}>{item.title}</a>
+    //         </Item>
+    //     )
+    // })
     return (
         <MenuContainer>
-            <MenuItems>{menuItems}</MenuItems>
+            <MenuItems>stuff</MenuItems>
         </MenuContainer>
     )
 }
