85b60a2811fc76ef2c4ed1f84e4439d3e563ca37
Rob Byrne <rob@leanrob.com>
2021-11-06 21:03:11 -0700

    Try removing middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,26 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest) {
-    const url = req.url
-    if (url.includes('api')) {
-        return NextResponse.next()
-    }
-
-    const basicAuth = req.headers.get('authorization')
-
-    if (basicAuth) {
-        const auth = basicAuth.split(' ')[1]
-        const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
-        if (user === 'admin' && pwd === 'its-a-me-mario') {
-            return NextResponse.next()
-        }
-    }
-
-    return new Response('Auth required', {
-        status: 401,
-        headers: {
-            'WWW-Authenticate': 'Basic realm="Secure Area"',
-        },
-    })
+    // const url = req.url
+    // if (url.includes('api')) {
+    //     return NextResponse.next()
+    // }
+    //
+    // const basicAuth = req.headers.get('authorization')
+    //
+    // if (basicAuth) {
+    //     const auth = basicAuth.split(' ')[1]
+    //     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+    //
+    //     if (user === 'admin' && pwd === 'its-a-me-mario') {
+    //
+    //     }
+    // }
+    //
+    // return new Response('Auth required', {
+    //     status: 401,
+    //     headers: {
+    //         'WWW-Authenticate': 'Basic realm="Secure Area"',
+    //     },
+    // })
+    return NextResponse.next()
 }
