404945b9b3b02a294af50a25e0471053a1be7bd7
Rob Byrne <rob@leanrob.com>
2021-11-07 01:23:52 -0800

    Try to fix menu items again

--- components/Home/Menu.tsx
+++ components/Home/Menu.tsx
@@ -4,13 +4,15 @@ import { MenuContainer, MenuItems, Item } from './HomeStyles'
 
 const Menu = ({ data }: any) => {
     const { menuItems } = data
-    const items = menuItems.map((item: any, index: number) => {
-        return (
-            <Item key={index}>
-                <a href={item.link}>{item.title}</a>
-            </Item>
-        )
-    })
+    const items =
+        menuItems &&
+        menuItems.map((item: any, index: number) => {
+            return (
+                <Item key={index}>
+                    <a href={item.link}>{item.title}</a>
+                </Item>
+            )
+        })
     return (
         <MenuContainer>
             <MenuItems>{items}</MenuItems>
