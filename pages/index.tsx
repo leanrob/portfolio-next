e32b4baa5df1044d38d66dc497f6ff601432e328
Rob Byrne <rob@leanrob.com>
2021-11-06 17:21:53 -0700

    Remove non-page file from the pages folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 const dotenv = require('dotenv')
 
-import { Page, Container } from './IndexStyles'
+import { Page, Container } from '../styles/IndexStyles'
 
 import {
     AboveTheFold,
