d166f60e5183d408f59a125192d1549e62747400
Rob Byrne <rob@leanrob.com>
2021-11-13 01:47:50 -0800

    Add to data api endpoint

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -2,8 +2,26 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import * as data from '@data/home.json'
 
 type Data = {
-    aboveTheFold: any
-    highLevelSkills: any
+    menu: {
+        menuItems: Array<{ title: string; link: string }>
+    }
+    aboveTheFold: {
+        header: string
+        subheader: string
+    }
+    highLevelSkills: {
+        header: string
+        subheader: string
+        skills: Array<{ title: string; content: string; link: string }>
+    }
+    languagesAndFrameworks: {
+        header: string
+        subheader: string
+        spinnerContent: {
+            backend: Array<{ image: string }>
+            frontend: Array<{ image: string }>
+        }
+    }
 }
 
 export default function handler(
